6cd945f5224679f742a35df6b2764e6b3eefb371
ctembe <ctembe@gmail.com>
2023-02-17 21:06:25 +0530

    change in index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //--
 
-const PORT = 3000;
+//const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // app.use((req, res, next) => {
 //     console.log('Time:', Date.now())
